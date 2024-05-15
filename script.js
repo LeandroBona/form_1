e33a258f5e3e80d08f3e4dc76fee75996a5315f3
Leandro Bona <42920804+LeandroBona@users.noreply.github.com>
2024-05-15 08:16:50 -0300

    atualizando!

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var gender = document.getElementById("gender").value;
+    var message = document.getElementById("message").value;
+
+    if (!name || !email || !gender || !message) {
+        alert("Por favor, preencha todos os campos.");
+        event.preventDefault();
+    } else if (!validateEmail(email)) {
+        alert("Por favor, insira um endereço de email válido.");
+        document.getElementById("email").style.borderColor = "red";
+        event.preventDefault();
+    }
+});
+
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
